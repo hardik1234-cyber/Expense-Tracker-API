dde5e6af6b99945bd353b811edfa13503b43166e
Hardik <hardikpandey10.hp@gmail.com>
2024-06-02 10:01:15 +0530

    UMS CRUD ops full working

--- xpense_tracker/schemas/ums/ums_model.py
+++ xpense_tracker/schemas/ums/ums_model.py
@@ -5,3 +5,11 @@ from sqlmodel import SQLModel
 class UserBaseModel(SQLModel):
     username: str
     email:str
+    class Config:
+        orm_mode = True
+
+class UserUpdateModel(SQLModel):
+    username: str
+    password: str
+    email: str
+    
