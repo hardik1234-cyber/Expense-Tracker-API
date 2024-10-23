2c5e7e286b45cf843326b1fea25ad3c0a5eaa5dc
Hardik <hardikpandey10.hp@gmail.com>
2024-10-23 19:25:51 +0530

    RENDER db URL CHNAGES

--- xpense_tracker/database/database_connection.py
+++ xpense_tracker/database/database_connection.py
@@ -1,6 +1,7 @@
 from sqlmodel import SQLModel,create_engine,Session
 
-DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
+# DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
+DB_URL = "postgresql://walter:nyBsH4HuV5k1bNEzSv7XYmyW9Ccsy5XV@dpg-cscfk1tds78s738phf0g-a.frankfurt-postgres.render.com/xpense_db_hcls"
 engine = create_engine(DB_URL,echo=True)
 
 def get_session():
