2cff0f1d92d2938a6ac65a3552510801add9fce7
Hardik <hardikpandey10.hp@gmail.com>
2024-11-01 22:34:01 +0530

    auth logging left rest done

--- xpense_tracker/database/database_connection.py
+++ xpense_tracker/database/database_connection.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel,create_engine,Session
 
 # DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
 DB_URL = "postgresql://walter:nyBsH4HuV5k1bNEzSv7XYmyW9Ccsy5XV@dpg-cscfk1tds78s738phf0g-a.frankfurt-postgres.render.com/xpense_db_hcls"
-engine = create_engine(DB_URL,echo=True)
+engine = create_engine(DB_URL)
 
 def get_session():
     with Session(engine) as session:
--- xpense_tracker/services/ExpenseManagementSystem/ems.py
+++ xpense_tracker/services/ExpenseManagementSystem/ems.py
@@ -4,15 +4,24 @@ from database.database_connection import get_session
 from database.tables import User,Expense
 from schemas.ems.ems_model import ExpenseDetails, ExpenseModel, ExpenseUpdateModel
 from services.authentication.oauth2 import get_current_user
+from logs.logging import logger
+
 
 ems_router = APIRouter(tags=['Expense Management System'])
 
 @ems_router.post('/add_expense',status_code=status.HTTP_201_CREATED)
 def add_expense(user:ExpenseModel ,get_logged_in_user = Depends(get_current_user),db: Session = Depends(get_session)):
     expense = Expense(username=user.username,amount=user.amount,category=user.category,description=user.description,date= user.date)
-    db.add(expense)
-    db.commit()
-    db.refresh(expense)
+    try:
+        db.add(expense)
+        db.commit()
+        logger.debug("Expense added to DB")
+    except Exception as e:
+        logger.error("Error occurred while adding expense")
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Failed to add expense")
+    finally:
+        db.refresh(expense)
     return "Expense added"
 
 @ems_router.get('/get_expense',status_code=status.HTTP_200_OK)
@@ -21,9 +30,10 @@ def get_expense(username: str,get_logged_in_user = Depends(get_current_user),db:
     expense = db.exec(select(Expense).where(Expense.username == username)).one_or_none()
 
     if expense is None:
+        logger.error("No expense Found")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found"
+            detail="expense not found"
         )
     
     return ExpenseDetails(amount=expense.amount,category=expense.category,description=expense.description,date=expense.date)
@@ -36,23 +46,33 @@ def update_expense(username: str,expense_model: ExpenseUpdateModel,get_logged_in
     expense = db.exec(select(Expense).where(Expense.username == username)).one_or_none()
 
     if expense is None:
+        logger.error("Expense not found")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found"
+            detail="Expense not found"
         )
     
     if expense_model.amount is not None:
+        logger.info("Updated amount")
         expense.amount = expense_model.amount
     if expense_model.description is not None:
+        logger.info("Updated description")
         expense.description = expense_model.description
     if expense_model.category is not None:
+        logger.info("Updated category")
         expense.category = expense_model.category
     if expense_model.date is not None:
+        logger.info("Updated date")
         expense.date = expense_model.date
-
-    db.add(expense)
-    db.commit()
-    db.refresh(expense)
+    try:
+        db.add(expense)
+        db.commit()
+        logger.info("expense updated")
+    except Exception as e:
+        db.rollback()
+        logger.debug("Unable to update expense")
+    finally:
+        db.refresh(expense)
 
 @ems_router.delete('/delete_expense',status_code=status.HTTP_204_NO_CONTENT)
 def delete_expense(username:str,get_logged_in_user = Depends(get_current_user),db: Session = Depends(get_session)):
@@ -60,13 +80,20 @@ def delete_expense(username:str,get_logged_in_user = Depends(get_current_user),d
     expense = db.exec(select(Expense).where(Expense.username == username)).one_or_none()
 
     if expense is None:
+        logger.warning("Expense not found")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found"
+            detail="Expense not found"
         )
     
-    db.delete(expense)
-    db.commit()
+    try:
+        db.delete(expense)
+        logger.info("Expense deleted")
+    except Exception as e:
+        logger.debug("error in deleting the expense")
+        db.rollback()
+    finally:
+        db.commit()
 
 
     
