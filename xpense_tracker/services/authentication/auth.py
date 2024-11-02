a6be74a3b36726b8a8247b8f2fc1f0b79215480b
Hardik <hardikpandey10.hp@gmail.com>
2024-11-02 10:02:00 +0530

    Auth logging done

--- xpense_tracker/services/authentication/auth.py
+++ xpense_tracker/services/authentication/auth.py
@@ -5,36 +5,53 @@ from database.tables import User
 from schemas.authentication.auth_model import Token, UserSignUp, UserLogin
 from services.authentication.utils import hash_pass,verify_pass
 from services.authentication.oauth2 import create_access_token
+from logs.logging import logger
 
 auth_router = APIRouter(tags=['Authentication'])
 
 
 @auth_router.post('/signup',status_code=status.HTTP_201_CREATED)
 def sign_up(user: UserSignUp, db: Session = Depends(get_session)):
+
     new_user = User(username=user.username,password=hash_pass(user.password),email=user.email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    db.close()
+
+    try:
+        logger.info("Adding the user in DB")
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+    except Exception as e:
+        logger.error("Unable to add user")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Unable to add user , try again")
+    finally:
+        db.close()
     return "User Registered"
 
 @auth_router.post('/login',response_model=Token)
 def login(user_creds:UserLogin,db: Session = Depends(get_session)):
+    
     user = db.exec(select(User).where(User.username == user_creds.username)).one_or_none()
 
     if user is None:
+        logger.error("Invalid user Credentials")
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid Credentials!"
         )
 
     if not verify_pass(user_creds.password,user.password):
+        logger.error("Invalid password Credentials")
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid Credentials!"
         )
     
-    access_token = create_access_token(data={"username": user.username})
-    db.close()
+    try:
+        access_token = create_access_token(data={"username": user.username})
+    except Exception as e:
+        logger.warning("Unable to login this user")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Unable to login this user")
+    finally:
+        db.close()
     return Token(access_token=access_token,token_type="bearer")
             
