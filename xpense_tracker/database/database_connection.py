0cebbd61575142b2b4291fd3869b7a4a112c8704
Hardik <hardikpandey10.hp@gmail.com>
2024-12-31 21:59:30 +0530

    prod db url change

--- xpense_tracker/database/database_connection.py
+++ xpense_tracker/database/database_connection.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel,create_engine,Session
 
-DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
-# DB_URL = "postgresql://expense_tracker_prod_db_user:yUlbsCXSlPe9WhrVwmDSe4yABYyj37lz@dpg-ctiqlhogph6c738blsag-a.frankfurt-postgres.render.com/expense_tracker_prod_db"
+# DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
+DB_URL = "postgresql://expense_tracker_prod_db_user:yUlbsCXSlPe9WhrVwmDSe4yABYyj37lz@dpg-ctiqlhogph6c738blsag-a.frankfurt-postgres.render.com/expense_tracker_prod_db"
 engine = create_engine(DB_URL)
 
 def get_session():
