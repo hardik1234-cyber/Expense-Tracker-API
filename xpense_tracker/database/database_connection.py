663cb4b00f4212a4ae89960db96570228fc6ea4a
Hardik <hardikpandey10.hp@gmail.com>
2025-05-21 13:48:48 +0530

    db deployment chganges

--- xpense_tracker/database/database_connection.py
+++ xpense_tracker/database/database_connection.py
@@ -1,7 +1,7 @@
-from sqlmodel import SQLModel,create_engine,Session
+from sqlmodel import create_engine,Session
 
-DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
-# DB_URL = "postgresql://pstgres:ZGOk2sv5HSY3CTovdMuruKla3aLKyw42@dpg-d03tvt7gi27c738d8v30-a.frankfurt-postgres.render.com/testdb_83lk"
+# DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
+DB_URL = "postgresql://pstgres:ZGOk2sv5HSY3CTovdMuruKla3aLKyw42@dpg-d03tvt7gi27c738d8v30-a/testdb_83lk"
 engine = create_engine(DB_URL)
 
 def get_session():
