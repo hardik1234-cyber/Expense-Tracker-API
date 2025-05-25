6965d5130cbf7d305dd7a9f861ab2c28d7f5e671
Hardik <hardikpandey10.hp@gmail.com>
2025-05-25 13:22:45 +0530

    prod db conn

--- xpense_tracker/database/database_connection.py
+++ xpense_tracker/database/database_connection.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine,Session
 
-DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
-# DB_URL = "postgresql://pstgres:ZGOk2sv5HSY3CTovdMuruKla3aLKyw42@dpg-d03tvt7gi27c738d8v30-a/testdb_83lk"
+# DB_URL = "postgresql://postgres:root@localhost:5432/testdb"
+DB_URL = "postgresql://pstgres:NouDsbHtIvwuUEQtCuQLZ3ZVr01vHQsB@dpg-d0pcnr6mcj7s73e07v20-a/testdb_ulvx"
 engine = create_engine(DB_URL)
 
 def get_session():
