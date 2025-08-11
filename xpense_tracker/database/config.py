f29ad0bda3ea3740a57d47a06462936d3b3c738f
Hardik <hardikpandey10.hp@gmail.com>
2025-08-11 05:45:42 +0530

    Loki setup done

--- xpense_tracker/database/config.py
+++ xpense_tracker/database/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM : str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    LOKI_ENDPOINT: str | None = None
 
     class Config:
         env_file = ".env"
--- xpense_tracker/logs/logging.py
+++ xpense_tracker/logs/logging.py
@@ -1,11 +1,20 @@
 import logging
+import logging_loki
+from multiprocessing import Queue
+from database.config import settings
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename='text.log',
-    format='%(asctime)s - %(levelname)s - %(message)s'
-
+loki_logs_handler  = logging_loki.LokiQueueHandler(
+    Queue(-1),
+    url=settings.LOKI_ENDPOINT,
+    tags={"application": "xpense_tracker"},
+    version="1",
 )
 
 logger = logging.getLogger(__name__)
+
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+uvicorn_access_logger.addHandler(loki_logs_handler)
+logging.getLogger().addHandler(loki_logs_handler) 
+
+
 
